94038dc0000732c1ba6fe1f8bf26c2dd80c2665b
John Gallardo <jgalla@microsoft.com>
2018-05-16 18:13:37 -0700

    Fix build break

--- NativeRuntimeEventSource/AsyncDooDad.cpp
+++ NativeRuntimeEventSource/AsyncDooDad.cpp
@@ -2,12 +2,14 @@
 #include "AsyncDooDad.h"
 #include "Person.h"
 
+using namespace std::chrono_literals;
+
 namespace winrt::NativeRuntimeEventSource::implementation
 {
     Windows::Foundation::IAsyncOperation<NativeRuntimeEventSource::Person> AsyncDooDad::GetPersonAsync(hstring const firstName, hstring const lastName)
     {
         co_await winrt::resume_background();
-        co_await winrt::resume_after(1s)
+        co_await winrt::resume_after(1s);
         co_return winrt::make<Person>(firstName, lastName );
     }
 }
